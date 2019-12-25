675e5ba8db588083da71a51fae65e8f533490f04
teezamani <teezamani@gmail.com>
2019-12-25 19:15:27 +0100

    All

--- js/app.js
+++ js/app.js
@@ -3,3 +3,12 @@ $('body').scrollspy({
     target: '#mainNav',
     offset: 56
   });
+
+  // NAVBAR CHANGE
+$(window).on("scroll", () => {
+  if ($(window).scrollTop()) {
+      $("nav").addClass("scroll-navvy");
+  } else {
+      $("nav").removeClass("scroll-navvy");
+  }
+});
