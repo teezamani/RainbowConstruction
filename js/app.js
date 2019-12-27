f5ede66b4668bf6845a6a60693aec96fd2f6bd8d
teezamani <teezamani@gmail.com>
2019-12-28 00:37:37 +0100

    all

--- js/app.js
+++ js/app.js
@@ -12,3 +12,28 @@ $(window).on("scroll", () => {
       $("nav").removeClass("scroll-navvy");
   }
 });
+
+  // TYPEWRITER
+  let typeString = [
+    "Design",
+    "Engineering",
+    "Construction",
+    "Project Management"
+];
+let i = 0;
+let count = 0;
+let selectedText = "";
+let text = "";
+(type = () => {
+    if (count == typeString.length) {
+        count = 0;
+    }
+    selectedText = typeString[count];
+    text = selectedText.slice(0, ++i);
+    document.getElementById("typing").innerHTML = text;
+    if (text.length === selectedText.length) {
+        count++;
+        i = 0;
+    }
+    setTimeout(type, 200);
+})();
